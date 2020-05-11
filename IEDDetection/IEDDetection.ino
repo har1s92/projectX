40fa18278cb3a8aa93b6b03e9bfdb3b53bef0c08
zwrig_000 <zwright14@gmail.com>
2020-05-10 21:59:53 -0600

    Updated Teensy IED program to check for saturation

--- IEDDetection/IEDDetection.ino
+++ IEDDetection/IEDDetection.ino
@@ -6,6 +6,11 @@ const int analogInPin = A0;
 // declare digital output pin
 const int digitalOutputPin = 0;
 
+// declare the number of loops before adjusting potentiometer
+const int saturationLoops = 10;
+
+int saturationCount=0;
+
 void setup() {
   // initialize pin mode
   pinMode(digitalOutputPin, OUTPUT);
@@ -14,15 +19,26 @@ void setup() {
 void loop() {
   // read the analog value on the analogInPin (value from 0 to 1023)
   int analogInput = analogRead(analogInPin);
+
+  if (isSaturated(analogInput)) {
+    saturationCount+=1;
+  }
+  else {
+    saturationCount=0;
+  }
+
+  if (saturationCount>=saturationLoops) {
+    adjustPotentiometer();
+  }
   
   // calculate the voltage on the analogInPin (3.22 mV per unit)
   int voltageInput = analogInput * 0.00322;
   
   // write the output of the threshold to the output pin
-  digitalWrite(digitalOutputPin, isAboveThreshold(analogInput));
+  digitalWrite(digitalOutputPin, isAboveVoltageThreshold(analogInput));
 }
 
-int isAboveThreshold(int analogReading) {
+int isAboveVoltageThreshold(int analogReading) {
   // set the analog thershold value
   int analogThreshold = 0;
 
@@ -33,4 +49,19 @@ int isAboveThreshold(int analogReading) {
   else {
     return LOW;
   }
+}
+
+bool isSaturated(int analogReading) {
+  int lowThreshold = 5;
+  int highThreshold = 1020;
+  if (analogReading<lowThreshold || analogReading>highThreshold) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void adjustPotentiometer() {
+  //need to fill this in with code to get better analog readings
 }
